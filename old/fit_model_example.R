d4969200e452faee7173dcf6173f61b91a287871
jhellewell14 <joel@ebi.ac.uk>
2025-07-17 08:50:55 +0100

    Tidying directory

--- old/fit_model_example.R
+++ old/fit_model_example.R
@@ -4,7 +4,8 @@ library(data.table)
 source("R_funcs/generate_data.R")
 
 # Read alignment
-data <- seqinr::read.alignment(file = "data/porB3.carriage.noindels.txt", format = "fasta")
+# data <- seqinr::read.alignment(file = "data/porB3.carriage.noindels.txt", format = "fasta")
+data <- seqinr::read.alignment(file = "~/Downloads/recO.aln.fas", format = "fasta")
 
 cores <- 8
 nchains <- 1
@@ -14,9 +15,9 @@ Sys.setenv(STAN_NUM_THREADS = cores)
 data_list <- generate_data(data, cores = cores)
 
 # data list
-data_list$theta <- 0.17
-data_list$omega <- 1
-data_list$kappa <- 1
+data_list$K <- 2
+data_list$om_priors <- c(1, 2.5)
+
 
 # Compile model
 mod_fit_test <- cmdstan_model("models/fit_omega.stan", 
@@ -25,10 +26,17 @@ mod_fit_test <- cmdstan_model("models/fit_omega.stan",
 fit_new <- mod_fit_test$sample(
   data = data_list,
   iter_warmup = 1000, 
-  iter_sampling = 1000,
-  threads_per_chain = 8,
+  iter_sampling = 500,
+  threads_per_chain = 4,
   fixed_param = FALSE,
   chains = 1, 
-  refresh = 50
+  refresh = 1
 )
+fit <- readRDS("~/Downloads/hierarchical_fit(1).RDS")
 
+library(bayesplot)
+mcmc_intervals(fit$draws("omega"))
+
+fit$summary()
+mcmc_intervals(fit$draws("omega"))
+fit$summary("omega_mean")
